621637bc8cbb72ad2db85ab8118883f76b59e9f7
3372126760-lang <3372126760@qq.com>
2026-09-09 06:35:29 +0800

    配置 GitHub Pages 自动部署

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/whiteout-bear-damage-model/",
   plugins: [react()],
   build: mode === "library" ? {
     lib: {
